ae0d3da6336c4cfe635cc4116ee179fb16a648fb
chyoura <wof526@sookmyung.ac.kr>
2024-04-05 17:21:48 +0900

    4/5 yura

--- Assets/Scripts/Yura/endingManage.cs
+++ Assets/Scripts/Yura/endingManage.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class endingManage : MonoBehaviour
 {
@@ -11,7 +12,6 @@ public class endingManage : MonoBehaviour
     public int excelNum = 0;
     string endingName;
     bool SceneEnd = false;
-    int endingNum = 1;
 
     public GameObject endBut;
     public Sprite[] endingImgList = new Sprite[40];
@@ -21,6 +21,7 @@ public class endingManage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        endingImg.enabled = false;
         endBut.SetActive(false);
         script1 = GameObject.Find("script").GetComponent<TypeEffect>();
         endingChoice();
@@ -31,6 +32,7 @@ public class endingManage : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) && !SceneEnd)
         {
+            endingImg.enabled = true;
             Debug.Log("click");
             printScript();
 
@@ -38,8 +40,13 @@ public class endingManage : MonoBehaviour
         else if(SceneEnd)
         {
             endBut.SetActive(true);
-            //버튼 활성화
         }
+    }
+
+    public void endButton()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("StartScene");
     }
 
     void printScript()
@@ -75,7 +82,7 @@ public class endingManage : MonoBehaviour
 
     void endingChoice()
     {
-        switch (endingNum) //SettleScript.endingNum
+        switch (SettleScript.endingNum) //SettleScript.endingNum
         {
             case 1:
                 endingName = "True:극적인 균형";
@@ -83,31 +90,31 @@ public class endingManage : MonoBehaviour
                 Debug.Log(excelNum);
                 break;
             case 2:
-                endingName = "반동분자";
+                endingName = "Bad1: 반동분자";
                 excelNum = findExcelNum(endingName);
                 break;
             case 3:
-                endingName = "정부의 끄나풀";
+                endingName = "Bad2: 정부의 끄나풀";
                 excelNum = findExcelNum(endingName);
                 break;
             case 4:
-                endingName = "빚쟁이";
+                endingName = "Bad3: 빚쟁이";
                 excelNum = findExcelNum(endingName);
                 break;
             case 5:
-                endingName = "파산";
+                endingName = "Bad4: 파산";
                 excelNum = findExcelNum(endingName);
                 break;
             case 6:
-                endingName = "충실한 협력자";
+                endingName = "good1: 충실한 협력자";
                 excelNum = findExcelNum(endingName);
                 break;
             case 7:
-                endingName = "반란 주도자";
+                endingName = "good2: 반란 주도자";
                 excelNum = findExcelNum(endingName);
                 break;
             case 8:
-                endingName = "이중 스파이";
+                endingName = "good3: 이중 스파이";
                 excelNum = findExcelNum(endingName);
                 break;
             case 0:
